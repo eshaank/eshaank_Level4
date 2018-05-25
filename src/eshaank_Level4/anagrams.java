a0f0f77bfba4de4751727b09df4662ac94b04ae3
Eshaan K <eshaankansagara@gmail.com>
2018-05-24 17:05:27 -0700

    Hello
    Asdf

--- src/eshaank_Level4/anagrams.java
+++ src/eshaank_Level4/anagrams.java
@@ -11,8 +11,7 @@ public class anagrams {
 	ArrayList<String> splits = new ArrayList<String>();
 	ArrayList<String> goodWords = new ArrayList<String>();
 	String word;
-	String wordPick;
-	Boolean yes = true;
+	String repeat;
 
 	public static void main(String[] args) {
 		anagrams a = new anagrams();
@@ -28,25 +27,35 @@ public class anagrams {
 
 	void anagramSearch() throws FileNotFoundException {
 		word = JOptionPane.showInputDialog("Type a word");
-		wordPick = word;
-		// System.out.println(word);
-		for (int i = 0; i < word.length(); i++) {
-			splits.add(word.substring(i, i + 1));
 
-		}
-
-		// System.out.println(splits.size());
 		Scanner scan = new Scanner(new File("src/eshaank_Level4/dictionary.txt"));
 		while (scan.hasNextLine()) {
 			String str = scan.nextLine();
-			for (int i = 0; i < splits.size(); i++) {
-				if (str.contains(splits.get(0)) && str.contains(splits.get(1)) && str.contains(splits.get(2))
-						&& str.contains(splits.get(3)) && str.contains(splits.get(4)) && str.contains(splits.get(5))
-						&& str.length() == word.length()) {
-					JOptionPane.showMessageDialog(null , "" + str);
-				}
+
+			if (containsChar(word, str) && str.length() == word.length()) {
+				//System.out.println(str);
 			}
 
 		}
+	}
+
+	boolean containsChar(String word, String str) {
+
+		for (int i = 0; i < word.length(); i++) {
+			for (int j = 0; j < splits.size(); j++) {
+				if (splits.subList(j, j+1).equals(splits.subList(j+1, j+2))) {
+					System.out.println("hi");
+				}
+					//System.out.println(repeat);
+				}
+			String letters = word.substring(i, i + 1);
+			splits.add(word.substring(i, i + 1));
+			if (!str.contains(letters)) {
+				return false;
+			}
+
+		}
+		return true;
+
 	}
 }
