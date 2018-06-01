91473bf44d27f298e8a6585b2455728cbb6f25d3
HelloThereGeneralKenobi <League@AdministorsiMac.attlocal.net>
2018-05-31 17:01:29 -0700

    DONE

--- src/eshaank_Level4/anagrams.java
+++ src/eshaank_Level4/anagrams.java
@@ -9,9 +9,9 @@ import javax.swing.JOptionPane;
 
 public class anagrams {
 	ArrayList<String> splits = new ArrayList<String>();
-	ArrayList<String> goodWords = new ArrayList<String>();
+	
 	String word;
-	String repeat;
+	
 
 	public static void main(String[] args) {
 		anagrams a = new anagrams();
@@ -33,7 +33,7 @@ public class anagrams {
 			String str = scan.nextLine();
 
 			if (containsChar(word, str) && str.length() == word.length()) {
-				//System.out.println(str);
+				System.out.println(str);
 			}
 
 		}
@@ -42,19 +42,21 @@ public class anagrams {
 	boolean containsChar(String word, String str) {
 
 		for (int i = 0; i < word.length(); i++) {
-			for (int j = 0; j < splits.size(); j++) {
-				if (splits.subList(j, j+1).equals(splits.subList(j+1, j+2))) {
-					System.out.println("hi");
-				}
-					//System.out.println(repeat);
-				}
 			String letters = word.substring(i, i + 1);
-			splits.add(word.substring(i, i + 1));
+
 			if (!str.contains(letters)) {
 				return false;
 			}
-
+			  if (str.contains(letters)) {
+				str = str.replaceFirst(letters, "");
+				
+				 //System.out.println(str);
+				 if (str.isEmpty() ) {
+					return true;
+				}
+			}
 		}
+
 		return true;
 
 	}
